35ebd4a2bc71aa4f9a98ecad82a7d72605dc45a3
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-03 11:37:35 +0530

    arrays

---
+++ scr/Descending.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Descending {
+    public static void main(String[] args) {
+        Double[]prices ={30.46,41.90,23.56,45.89,20.00};
+        Arrays.sort(prices,Collections.reverseOrder());
+        System.out.println("list in descending order");
+        for(Double price:prices)
+        {
+            System.out.println(price);
+        }
+    }
+    
+}
