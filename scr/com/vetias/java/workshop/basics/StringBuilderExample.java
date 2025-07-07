814df0770a8128aa2e16416c6c2bfafa5060e893
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-07 14:24:05 +0530

    StringBuilder.java

---
+++ scr/com/vetias/java/workshop/basics/StringBuilderExample.java
@@ -0,0 +1,19 @@
+package com.vetias.java.workshop.basics;
+
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Java");
+        sb.append(", ").append("is");
+        sb.append(", ").append("the");
+        sb.append(", ").append("most");
+        sb.append(", ").append("used");
+        sb.append(", ").append("language");
+        sb.append(", ").append("for");
+        sb.append(", ").append("Enterprise");
+        sb.append(", ").append("Applications");
+        System.out.println(sb.toString());
+        
+    }
+
+}
--- scr/com/vetias/java/workshop/basics/Stringc.java
+++ scr/com/vetias/java/workshop/basics/Stringc.java
@@ -4,7 +4,7 @@ public class Stringc {
     public static void main(String[] args) {
         String sentance = "This is program shows how can we split a " +
                     "string into multiple string.We need to find sentance," +
-                     "word and letter count \"; ";
+                     "word and letter count ";
         String words[]=sentance.split(" ");
         String sentances[]=sentance.split("\\.");
         System.out.println(words.length);
