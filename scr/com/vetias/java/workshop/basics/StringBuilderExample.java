7d5efacdfb213b559560cd40b0be46f1d21bbc63
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-07 14:34:19 +0530

    StringBuilder.java

--- scr/com/vetias/java/workshop/basics/StringBuilderExample.java
+++ scr/com/vetias/java/workshop/basics/StringBuilderExample.java
@@ -4,14 +4,14 @@ public class StringBuilderExample {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         sb.append("Java");
-        sb.append(", ").append("is");
-        sb.append(", ").append("the");
-        sb.append(", ").append("most");
-        sb.append(", ").append("used");
-        sb.append(", ").append("language");
-        sb.append(", ").append("for");
-        sb.append(", ").append("Enterprise");
-        sb.append(", ").append("Applications");
+        sb.append("  ").append("is");
+        sb.append("  ").append("the");
+        sb.append("  ").append("most");
+        sb.append("  ").append("used");
+        sb.append("  ").append("language");
+        sb.append("  ").append("for");
+        sb.append(" ").append("Enterprise");
+        sb.append(" ").append("Applications");
         System.out.println(sb.toString());
         
     }
