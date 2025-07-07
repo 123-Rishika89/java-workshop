4682a4ac9ced6347b1eb8e4c6f867701006604c0
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-07 15:20:27 +0530

    nameGenerated.java

---
+++ scr/com/vetias/java/workshop/basics/NameGenerated.java
@@ -0,0 +1,24 @@
+package com.vetias.java.workshop.basics;
+import java.util.Scanner;
+public class NameGenerated {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter the name:");
+        String Name=input.next();
+        System.out.println("enter the gender:");
+        String gender=input.next();
+        System.out.println("enter the qualification");
+        String qualification=input.next();
+        StringBuilder sb=new StringBuilder();
+        if ("female".equalsIgnoreCase(gender)) {
+            sb.append("Miss.").append (gender);
+            String result=sb.toString();
+            System.out.println(result);
+        } else {
+            sb.append("Mr.").append(gender);
+            String result=sb.toString();
+            System.out.println(result);   
+        }  
+    }
+
+}
