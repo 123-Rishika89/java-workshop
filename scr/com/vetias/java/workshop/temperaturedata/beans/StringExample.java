6f85ffc09e1a7f64c20bfdd079a92201b402c2dd
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-07 10:42:02 +0530

    StringExample.java

--- scr/com/vetias/java/workshop/temperaturedata/beans/StringExample.java
+++ scr/com/vetias/java/workshop/temperaturedata/beans/StringExample.java
@@ -3,13 +3,17 @@ package com.vetias.java.workshop.temperaturedata.beans;
 public class StringExample {
     public static void main(String[] args) {
         String firstName = "Rishika";
-        String lastName = new String ("Vishnu");
+        
         String shortName = "Rishi";
         String buddyName = new String ("Rishika");
-        System.out.println(firstName);
-        String fullName = firstName+""+lastName;
-        String finalName = firstName.concat(lastName);
-        System.out.println(finalName.toUpperCase());
+        System.out.println(firstName == shortName);
+        // System.out.println(firtName == bubbyName);
+        // String fullName = firstName+""+lastName;
+        // String finalName = firstName.concat(lastName);
+    //     System.out.println(finalName.toUpperCase());
+        String sentance = "This is program shows how can we spilt a string into multiple string";
+        String words[] = sentance.split("");
+        System.out.println(words.length);
     }
 
 }
