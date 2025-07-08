153d0a3d266e3bef2c69c7b7df07d03fc55833ca
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-08 15:05:04 +0530

    calculator.java

--- scr/com/vetias/java/workshop/basics/Calculator.java
+++ scr/com/vetias/java/workshop/basics/Calculator.java
@@ -1,15 +1,26 @@
 package com.vetias.java.workshop.basics;
 
 public class Calculator {
-    public static void main(String[]args){
-        int a =50;
-        int b =46;
+    
+    public static void main(String[] args) {
+        int a=3,b=0;
         System.out.println(a+b);
-        System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
-        System.out.println(a%b);
+        try{
+            if (b==0) 
+            {
+                throw new ArithmeticException("0 cant be a divisor");
+
+                
+            }
+        }catch(ArithmeticException e){
+                System.out.println(e.getMessage());
+        }
+            
+    }
 
 }
 
-}
+    
+  
