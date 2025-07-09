dd49c2fe95ee410425d2ba609b444aef5f8cff92
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-09 11:18:24 +0530

    FileCopyUsingImage.java

--- scr/com/vetias/java/workshop/Standardlib/FileCopyUsingBinary.java
+++ scr/com/vetias/java/workshop/Standardlib/FileCopyUsingBinary.java
@@ -6,8 +6,8 @@ import java.io.OutputStream;
 
 public class FileCopyUsingBinary {
     public static void main(String[] args) {
-        try(FileInputStream inputStream = new FileInputStream("dept csa.PNG");
-            FileOutputStream outputStream = new FileOutputStream("dept csa.PNG") )
+        try(FileInputStream inputStream = new FileInputStream("Capture.PNG");
+            FileOutputStream outputStream = new FileOutputStream("Rishika.PNG") )
             {
                 byte[] chunk = new byte[1024];
                 int byteRead;
