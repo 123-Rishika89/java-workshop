9507d8d87bec08ebf4a3aebb335e5618550334f9
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-11 09:31:02 +0530

    Student.java

--- src/main/java/com/vetias/java/Student.java
+++ src/main/java/com/vetias/java/Student.java
@@ -4,16 +4,16 @@ public class Student {
 
     public static void main(String[]args){
         String[] [] students =
-    {
-        {"01","sowme"},{"02","arthi"},{"03","mitra"},{"04","karthi"}
+        {
+           {"01","sowme"},{"02","arthi"},{"03","mitra"},{"04","karthi"}
 
-    };
-    System.out.println("roll number and name");
-    for(int i=0;i<students.length;i++)
-    {
-        System.out.printf(students[i][0],students[i][i]);
+        };
+        System.out.println("roll number and name");
+        for(int i=0;i<students.length;i++)
+        {
+            System.out.printf("%s%n%s%n", students[i][0],students[i][i]);
 
-    }
+        }
     }
     
 }
