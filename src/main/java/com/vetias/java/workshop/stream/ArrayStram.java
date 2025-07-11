b9a4da32ed68cfd68c95e260ec6013bccc12dce2
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-11 14:41:04 +0530

    EvenOddSplitExample.java

--- src/main/java/com/vetias/java/workshop/stream/ArrayStram.java
+++ src/main/java/com/vetias/java/workshop/stream/ArrayStram.java
@@ -1,6 +1,7 @@
 package com.vetias.java.workshop.stream;
 import java.util.Arrays;
 import java.util.Stream.IntStream;
+
 public class ArrayStram {
     public static void main(String[] args) {
         int[] marks ={90,100,55,30,87};
@@ -10,7 +11,7 @@ public class ArrayStram {
 
         IntStream markStream = Arrays.stream(marks);
         numberOfSubjectsPassed = markStream
-        .filter(mark -> mark> 40).count();
+        .filter(mark -> mark > 40).count();
     }
 
 }
