9b88df7fc28f9986dfab0b8e3016c5b0f7dfcd0a
123-Rishika89 <rishikasuresh04@gmail.com>
2025-07-11 13:45:26 +0530

    StreamUpperCaseNames.java

---
+++ src/main/java/com/vetias/java/workshop/collections/StreamUpperCaseNames.java
@@ -0,0 +1,22 @@
+package com.vetias.java.workshop.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamUpperCaseNames {
+    public static void main(String[] args) {
+        List<String>names = new ArrayList<>();
+        names.add("Rishika");
+        names.add("Pramisha");
+        names.add("Dhanushya");
+        names.add("Srinithi");
+        names.add("Sree vishnuprirya");
+        names.stream()
+        .filter(name -> name.startsWith("S") || name.startsWith("s"))
+        .distinct().map(String::toUpperCase).forEach(System.out::println);
+        
+
+
+}
+}
+
